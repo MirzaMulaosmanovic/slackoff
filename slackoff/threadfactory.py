71febf655b8b8324c2c8e4f20008ce7fe5dfe8bd
Mirza Mulaosmanovic <m.mulaosmanovic@shipworks.com>
2016-08-26 09:51:34 -0500

    stop storing threads

--- slackoff/threadfactory.py
+++ slackoff/threadfactory.py
@@ -1,14 +1,10 @@
 import threading
 
-threads = []
-
 def default_factory(target, name=None, as_daemon=True, args=None):
     """
     default thread factory, used to create threads as daemon
     """
     t = threading.Thread(target=target, name=name, args=args or [])
     t.setDaemon(as_daemon)
-
-    threads.append(t)
     return t
     
